241d28190ca5bcf1bb4ae32146cda3b521bde6f0
jimmy <jimmyhd_19@hotmail.com>
2024-03-23 19:44:26 -0500

    solucion final

--- src/app/padre-con-dynamic-dialog/padre-con-dynamic-dialog.component.ts
+++ src/app/padre-con-dynamic-dialog/padre-con-dynamic-dialog.component.ts
@@ -1,46 +1,46 @@
-import {Component, inject} from '@angular/core';
-import {ButtonModule} from "primeng/button";
-import {DialogModule} from "primeng/dialog";
-import {HijoComponent} from "../shared/hijo/hijo.component";
-import {InputTextModule} from "primeng/inputtext";
-import {PaginatorModule} from "primeng/paginator";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
-import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { HijoComponent } from '../shared/hijo/hijo.component';
 
 @Component({
-  selector: 'app-padre-con-dynamic-dialog',
-  standalone: true,
-    imports: [
-        ButtonModule,
-        DialogModule,
-        HijoComponent,
-        InputTextModule,
-        PaginatorModule,
-        ReactiveFormsModule
-    ],
-  templateUrl: './padre-con-dynamic-dialog.component.html',
-  styleUrl: './padre-con-dynamic-dialog.component.scss',
-  providers: [DialogService]
+	selector: 'app-padre-con-dynamic-dialog',
+	standalone: true,
+	imports: [ButtonModule, DialogModule, HijoComponent, InputTextModule, PaginatorModule, ReactiveFormsModule],
+	templateUrl: './padre-con-dynamic-dialog.component.html',
+	styleUrl: './padre-con-dynamic-dialog.component.scss',
+	providers: [DialogService]
 })
 export class PadreConDynamicDialogComponent {
-  private _formBuilder = inject(FormBuilder);
+	private _formBuilder = inject(FormBuilder);
 
-  ref: DynamicDialogRef | undefined;
-  dialogService = inject(DialogService)
+	ref: DynamicDialogRef | undefined;
+	dialogService = inject(DialogService);
 
-  petDialog: boolean = false;
+	petDialog: boolean = false;
 
-  formGroup = this._formBuilder.group({
-    name: ['Jonathan'],
-    age: [24],
-    tool: ['Laptop'],
-    pet: this._formBuilder.group({
-      name: ['Perlita'],
-      color: ['Gray']
-    }),
-  });
+	formGroup = this._formBuilder.group({
+		name: ['Jonathan'],
+		age: [24],
+		tool: ['Laptop'],
+		pet: this._formBuilder.group({
+			name: ['Perlita'],
+			color: ['Gray']
+		})
+	});
 
-  showDialog() {
-    this.ref = this.dialogService.open(HijoComponent, { header: 'Pet Details'});
-  }
+	showDialog() {
+		this.ref = this.dialogService.open(HijoComponent, {
+			header: 'Pet Details',
+			data: { pet: this.formGroup.controls.pet }
+		});
+	}
+
+	show() {
+		console.log(this.formGroup.value);
+	}
 }
--- src/app/shared/hijo/hijo.component.ts
+++ src/app/shared/hijo/hijo.component.ts
@@ -1,34 +1,28 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
-import {ButtonModule} from "primeng/button";
-import {DialogModule} from "primeng/dialog";
-import {InputTextModule} from "primeng/inputtext";
-import {ControlContainer, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import { Component, inject, OnInit } from '@angular/core';
+import { ControlContainer, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-hijo',
-  standalone: true,
-    imports: [
-        ButtonModule,
-        DialogModule,
-        InputTextModule,
-        ReactiveFormsModule
-    ],
-  templateUrl: './hijo.component.html',
-  styleUrl: './hijo.component.scss'
+	selector: 'app-hijo',
+	standalone: true,
+	imports: [ButtonModule, DialogModule, InputTextModule, ReactiveFormsModule],
+	templateUrl: './hijo.component.html',
+	styleUrl: './hijo.component.scss'
 })
-export class HijoComponent implements OnInit{
+export class HijoComponent implements OnInit {
+	private parentContainer = inject(ControlContainer, { optional: true });
+	formGroup!: FormGroup;
 
-  private parentContainer = inject(ControlContainer);
-  formGroup!: FormGroup;
+	private readonly _dynamicDialogConfig: DynamicDialogConfig<{ pet: FormGroup }> = inject(DynamicDialogConfig);
 
-  ngOnInit(): void {
-    this.formGroup = this.parentFormGroup;
-  }
+	ngOnInit(): void {
+		this.formGroup = this.parentFormGroup ? this.parentFormGroup : this._dynamicDialogConfig.data!.pet;
+	}
 
-  get parentFormGroup() {
-    return this.parentContainer.control?.get('pet') as FormGroup;
-  }
-
-
-
+	get parentFormGroup() {
+		return this.parentContainer?.control?.get('pet') as FormGroup;
+	}
 }
